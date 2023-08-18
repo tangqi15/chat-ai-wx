be7fbb4dbe93dc539a5d5d56ce00b0a7a6cecb10
tangqi <616992443@qq.com>
2023-08-18 18:57:17 +0800

    feat: 修改样式

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <!-- <view class="container" :style="{ backgroundImage: 'url(' + imageURL + ')' }"> -->
+  <!-- <view class="container" :style="{ backgroundImage: 'url(' + modelUrl + ')' }"> -->
   <view class="container">
     <view class="content">
       <view class="headerButton">
-        <van-button size="mini" type="info" @click="settingParameters"
-          >当前设置</van-button
-        >
-        <!-- <van-button size="mini" type="info" @click="settingMineInfo"
-          >个人中心</van-button
+        <!-- <van-button size="mini" type="info" @click="settingParameters"
+          > -->
+          <!-- 当前设置 -->
+          <image class="settingImg" src="@/static/imgs/setting.png" @click="settingParameters"></image>
+          <!-- </van-button
         > -->
+        <van-button size="mini" type="info" @click="settingMineInfo"
+          >个人中心</van-button
+        >
       </view>
       <!-- 人物模型 -->
-      <view class="model" hover-class="none" hover-stop-propagation="false">
-        <image class="modelImage" :src="modelUrl"></image>
+      <view class="model" :style="{ backgroundImage: 'url(' + modelUrl + ')' }">
+        <!-- <image class="modelImage" :src="modelUrl"></image> -->
         <!-- <van-image
           fit="cover"
           class="modelImage"
@@ -22,36 +25,7 @@
 
       <!-- 人物参数 -->
       <view class="characterProperty">
-        <view v-if="registerShow" class="registerMenu">
-          <!-- 人物属性 -->
-          <view class="" @click="characterAttributes">
-            <image
-              class="rowImage"
-              src="@/static/imgs/character/characterMan1.png"
-            ></image>
-          </view>
-          <!-- 声音 -->
-          <view class="" @click="characterSound">
-            <image
-              class="rowImage"
-              src="@/static/imgs/character/characterMan1.png"
-            ></image>
-          </view>
-          <!-- 性格 -->
-          <view class="" @click="characterCharacter">
-             <image
-              class="rowImage"
-              src="@/static/imgs/character/characterMan1.png"
-            ></image>
-          </view>
-          <!-- AI的经历描述 -->
-          <view class="" @click="characterExperience">
-             <image
-              class="rowImage"
-              src="@/static/imgs/character/characterMan1.png"
-            ></image>
-          </view>
-        </view>
+       
 
         <!-- 人物 (男、女 )  、
       声音（方言、普通话， 声音类型（例如: 御女、萝莉）可以试听 ）
@@ -64,7 +38,10 @@
           hover-stop-propagation="false"
           v-if="characterAttributesShow"
         >
-          <view> 男 女 </view>
+          <van-radio-group v-model="sexRadio" class="sexStyle">
+            <van-radio name="1"><view class="colorWhite">女</view></van-radio>
+            <van-radio name="2"><view class="colorWhite">男</view></van-radio>
+          </van-radio-group>
         </view>
         <!-- 声音模块 -->
         <view
@@ -74,7 +51,61 @@
           v-if="soundShow"
         >
           <!-- 这里放一个声音列表 -->
-          <view></view>
+          <view>
+            <!-- 方言 -->
+            <!-- 方言还是普通话 -->
+            <view class="soundRadio">
+              <van-radio-group v-model="radio" @click="soundRadioClick">
+                <van-radio name="1"><view class="colorWhite">方言</view></van-radio>
+                <van-radio name="2"><view class="colorWhite">普通话</view></van-radio>
+              </van-radio-group>
+            </view>
+
+            <!-- 声音类型   御女、萝莉 -->
+            <!-- 御女 -->
+            <view class="soundType">
+              <view
+                :class="[sexualWomanShow ? 'sexualBorder' : '']"
+                @click="sexualClick"
+              >
+                <image class="rowImage" src="@/static/imgs/sound/type1.webp">
+                </image>
+                <view
+                  class=""
+                  hover-class="none"
+                  hover-stop-propagation="false"
+                >
+                  御女
+                </view>
+              </view>
+              <!-- 萝莉 -->
+              <view :class="loriShow ? 'sexualBorder' : ''" @click="loriClick">
+                <image
+                  class="rowImage"
+                  src="@/static/imgs/sound/type2.jpeg"
+                ></image>
+                <view
+                  class=""
+                  hover-class="none"
+                  hover-stop-propagation="false"
+                >
+                  萝莉
+                </view>
+              </view>
+              <!-- 大妈 -->
+              <view :class="auntShow ? 'sexualBorder' : ''" @click="auntClick">
+                <image class="rowImage" src="@/static/imgs/sound/type3.webp">
+                </image>
+                <view
+                  class=""
+                  hover-class="none"
+                  hover-stop-propagation="false"
+                >
+                  大妈
+                </view>
+              </view>
+            </view>
+          </view>
         </view>
         <!-- 性格模块 -->
         <view
@@ -84,7 +115,8 @@
           v-if="characterShow"
         >
           <!--  -->
-          <view>你暴躁么</view>
+          <view>例如：暴躁人</view>
+          你希望他是什么样的性格:<input type="text" />
         </view>
         <!-- 经历描述模块 -->
         <view
@@ -95,32 +127,65 @@
         >
           你希望他有什么经历:<input type="text" />
         </view>
-      </view>
-      <!-- <tabBar :active="0"></tabBar> -->
-      <view class="footButton">
+
+
+
+        <view v-if="registerShow" class="registerMenu">
+          <!-- 人物属性 -->
+          <view  :class="['rowImageBox', characterAttributesShow ? 'selectedStyle' : '']" @click="characterAttributes">
+            <image
+              :class="['rowImage']"
+              src="@/static/imgs/character/characterMan1.png"
+            ></image>
+          </view>
+          <!-- 声音 -->
+          <view  :class="['rowImageBox', soundShow ? 'selectedStyle' : '']" @click="characterSound">
+            <image class="rowImage" src="@/static/imgs/sound/sound3.png"></image>
+          </view>
+          <!-- 性格 -->
+          <view  :class="['rowImageBox', characterShow ? 'selectedStyle' : '']" @click="characterCharacter">
+            <image
+              class="rowImage"
+              src="@/static/imgs/makeUp/makeUp4.png"
+            ></image>
+          </view>
+          <!-- AI的经历描述 -->
+          <view  :class="['rowImageBox', experienceShow ? 'selectedStyle' : '']" @click="characterExperience">
+            <image
+              class="rowImage"
+              src="@/static/imgs/experience/experience2.png"
+            ></image>
+          </view>
+        </view>
+        <view v-if="!registerShow" class="registerMenu">
         <view @click="chatVoice">语音聊天</view>
         <view @click="chatFont">文字聊天</view>
       </view>
+      </view>
+     
+      
+       <tabBar :active="0"></tabBar>
     </view>
   </view>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-// import tabBar from "@/wxcomponents/common/tab-bar.vue";
+import tabBar from "@/wxcomponents/common/tab-bar.vue";
 export default Vue.extend({
   components: {
-    // tabBar,
+    tabBar,
   },
   data() {
     return {
       href: "https://uniapp.dcloud.io/component/README?id=uniui",
-      imageURL: "/static/imgs/lisa.png",
+      imageURL: "/static/imgs/lisa2.png",
       // src="https://img.yzcdn.cn/vant/cat.jpeg"
       // imageURL: "",
       img: "",
       registerShow: true,
-      radio: "1",
+      radio: "1", // 是否方言
+      sexRadio: '2', // 性别男女
       fromUserFace: uni.getStorageSync("userFace"),
       slider: 30, // 滑块
 
@@ -129,8 +194,11 @@ export default Vue.extend({
       characterShow: false, // 性格模块
       experienceShow: false, // 经历描述模块
 
-
       modelUrl: "/static/imgs/lisa2.png",
+
+      sexualWomanShow: false,
+      loriShow: false,
+      auntShow: false,
     };
   },
   computed: {
@@ -143,7 +211,9 @@ export default Vue.extend({
   },
   methods: {
     settingParameters() {
+      this.resetShow();
       this.registerShow = !this.registerShow;
+      
     },
     settingMineInfo() {
       uni.navigateTo({ url: `/pages/mine/index` });
@@ -163,26 +233,66 @@ export default Vue.extend({
 
     // 人物属性模块
     characterAttributes() {
+      let temp = this.characterAttributesShow;
       this.resetShow();
-      this.characterAttributesShow = true;
+      this.characterAttributesShow = temp;
+      this.characterAttributesShow = !this.characterAttributesShow;
     },
+    // 声音模块点击
     characterSound() {
+      let temp = this.soundShow;
       this.resetShow();
-      this.soundShow = true;
+      this.soundShow = temp;
+      this.soundShow = !this.soundShow;
     },
+    // 性格模块点击
     characterCharacter() {
+      let temp = this.characterShow;
       this.resetShow();
-      this.characterShow = true;
+      this.characterShow = temp;
+      this.characterShow = !this.characterShow;
     },
+    // AI的经历描述
     characterExperience() {
+      let temp = this.experienceShow;
       this.resetShow();
-      this.experienceShow = true;
+      this.experienceShow = temp;
+      this.experienceShow = !this.experienceShow;
     },
     resetShow() {
       this.soundShow = false;
       this.characterShow = false;
       this.experienceShow = false;
       this.characterAttributesShow = false;
+    },
+    soundRadioClick(val: any) {
+      console.log(val, "val");
+    },
+    // 御女类型点击
+    sexualClick() {
+      let temp = this.sexualWomanShow;
+      this.resetSound();
+      this.sexualWomanShow = temp;
+      this.sexualWomanShow = !this.sexualWomanShow;
+    },
+    // 萝莉类型点击
+    loriClick() {
+      let temp = this.loriShow;
+      this.resetSound();
+      this.loriShow = temp;
+      this.loriShow = !this.loriShow;
+    },
+    // 大妈类型点击
+    auntClick() {
+      let temp = this.auntShow;
+      this.resetSound();
+      this.auntShow = temp;
+      this.auntShow = !this.auntShow;
+    },
+    resetSound() {
+      this.loriShow = false;
+      this.auntShow = false;
+      this.sexualWomanShow = false;
     },
   },
 });
@@ -215,9 +325,16 @@ export default Vue.extend({
 
 // 人物参数
 .characterProperty {
-  flex: 1;
+  position: absolute;
+  bottom: 40px;
+  // left: 100px;
+  width: 100%;
+  background-color: rgb(85 0 0 / 40%);
+  color: white;
+  // border-radius: 30px 30px 0px 0px;
+  // flex: 1;
   // margin-top: 60vh;
-  // background-color: red;
+  // background: opacity;
 }
 
 // 人物参数 确认
@@ -240,6 +357,7 @@ export default Vue.extend({
 .footButton {
   display: flex;
   justify-content: space-between;
+  margin-bottom: 20px;
 }
 
 .propItemClass {
@@ -252,7 +370,6 @@ export default Vue.extend({
 .rowImage {
   width: 80rpx;
   height: 80rpx;
-  border-radius: 10rpx;
 }
 
 .rowSlider {
@@ -262,20 +379,66 @@ export default Vue.extend({
 
 // 人物模型
 .model {
-  // border: 1px solid darkmagenta;
+  // TODO  渐变色
+  background-color: rgb(176, 160, 198);
   width: 100%;
-  // height: 100%;
-  // padding: 10px;
-  // margin: 10px;
+  height: 100%;
+  background-position: center;
+  background-origin: content-box;
 }
-
 .modelImage {
-  // height: 60%;
   width: 100%;
+  height: 380px;
 }
 
 .registerMenu {
   display: flex;
   justify-content: space-around;
+  margin: 10px 0px;
+}
+
+.soundRadio {
+  display: flex;
+}
+.soundType {
+  display: flex;
+  justify-content: space-around;
+}
+
+.sexualBorder {
+  border: 10px solid gray;
+  border-radius: 10px;
+}
+
+.sexStyle {
+  display: flex;
+}
+
+
+.rowImageBox {
+  // box-sizing: border-box;
+  border: 2px solid rgba(85, 0, 0, 0);
+  padding: 2px;
+}
+
+.selectedStyle {
+  // border: 10px solid rgb(176, 160, 198);
+  // border: 10px solid rgb(176, 160, 198);
+  border-color: rgb(176, 160, 198);
+  border-radius: 30px;
+  background-color: rgb(176, 160, 198);
+}
+
+
+
+.settingImg {
+  width: 50rpx;
+  height: 50rpx;
+  z-index: 999;
+}
+
+
+.colorWhite {
+  color: white;
 }
 </style>
--- src/wxcomponents/common/tab-bar.vue
+++ src/wxcomponents/common/tab-bar.vue
@@ -1,39 +1,69 @@
 <template>
   <van-tabbar :active="active" placeholder fixed>
     <van-tabbar-item @click="homePicked">
-      <image slot="icon" :src="icon[0].normal" mode="aspectFit" class="itemBar" />
-      <image slot="icon-active" :src="icon[0].active" mode="aspectFit" class="itemBar" />
-      主页
+      <image
+        slot="icon"
+        :src="icon[0].normal"
+        mode="aspectFit"
+        class="itemBar"
+      />
+      <image
+        slot="icon-active"
+        :src="icon[0].active"
+        mode="aspectFit"
+        class="itemBar"
+      />
+      模型
     </van-tabbar-item>
     <van-tabbar-item @click="contentPicked">
-      <image slot="icon" :src="icon[1].normal" mode="aspectFit" class="itemBar" />
-      <image slot="icon-active" :src="icon[1].active" mode="aspectFit" class="itemBar" />
-      内容
+      <image
+        slot="icon"
+        :src="icon[1].normal"
+        mode="aspectFit"
+        class="itemBar"
+      />
+      <image
+        slot="icon-active"
+        :src="icon[1].active"
+        mode="aspectFit"
+        class="itemBar"
+      />
+      模型圈友
     </van-tabbar-item>
     <van-tabbar-item @click="minePicked">
-      <image slot="icon" :src="icon[2].normal" mode="aspectFit" class="itemBar" />
-      <image slot="icon-active" :src="icon[2].active" mode="aspectFit" class="itemBar" />
+      <image
+        slot="icon"
+        :src="icon[2].normal"
+        mode="aspectFit"
+        class="itemBar"
+      />
+      <image
+        slot="icon-active"
+        :src="icon[2].active"
+        mode="aspectFit"
+        class="itemBar"
+      />
       我的
     </van-tabbar-item>
   </van-tabbar>
 </template>
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
       icon: [
-	  {
-		normal: '/static/imgs/home.png',
-		active: '/static/imgs/homeselect.png',
-	  },
         {
-          normal: '/static/imgs/content.png',
-          active: '/static/imgs/contentselect.png',
+          normal: "/static/imgs/home.png",
+          active: "/static/imgs/homeselect.png",
         },
         {
-          normal: '/static/imgs/mine.png',
-          active: '/static/imgs/mineselect.png',
+          normal: "/static/imgs/content.png",
+          active: "/static/imgs/contentselect.png",
+        },
+        {
+          normal: "/static/imgs/mine.png",
+          active: "/static/imgs/mineselect.png",
         },
       ],
     };
@@ -46,33 +76,37 @@ export default Vue.extend({
       },
     },
   },
-  components: {
-  },
-  computed: {
-  },
+  components: {},
+  computed: {},
   methods: {
-	  // 首页
+    // 首页
     homePicked() {
       uni.switchTab({
-        url: '/pages/home/index',
+        url: "/pages/home/index",
       });
     },
     contentPicked() {
       uni.switchTab({
-        url: '/pages/content/index',
+        url: "/pages/content/index",
       });
     },
     minePicked() {
       uni.switchTab({
-        url: '/pages/mine/index',
+        url: "/pages/mine/index",
       });
     },
   },
 });
 </script>
-<style lang="scss">
-	.itemBar {
-		width: 30px;
-		height: 18px
-	}
+<style lang="scss" scoped>
+.itemBar {
+  width: 30px;
+  height: 18px;
+  border: 5px solid white;
+}
+
+.tabBarContent {
+  height: 100px;
+  background-color: black;
+}
 </style>
